d1fd45f27e518e116a39649782bb034519c1f942
mwyzer <muhammad.wyzer@gmail.com>
2023-02-05 01:56:41 +0700

    dashboard

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,9 +1,33 @@
 import Dashboard from "./components/Dashboard.vue";
+import ListAppointments from "./components/pages/appointments/ListAppointments.vue";
+import ListUsers from "./components/pages/users/ListUsers.vue";
+import UpdateSetting from "./components/pages/settings/UpdateSetting.vue";
+import UpdateProfile from "./components/pages/profile/UpdateProfile.vue";
 
 export default [
     {
-        path: "admin/dashboard",
+        path: "/admin/dashboard",
         name: "admin.dashboard",
         component: Dashboard,
+    },
+    {
+        path: "/admin/appointments",
+        name: "admin.appointments",
+        component: ListAppointments,
+    },
+    {
+        path: "admin/users",
+        name: "admin.users",
+        component: ListUsers,
+    },
+    {
+        path: "/admin/settings",
+        name: "admin.settings",
+        component: UpdateSetting,
+    },
+    {
+        path: "/admin/profile",
+        name: "admin.profile",
+        component: UpdateProfile,
     },
 ];
